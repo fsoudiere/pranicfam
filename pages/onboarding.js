8e74635b71c3b09c3edb0c2c0618e2b9368019fc
Fabien Soudiere <fabiensoudiere@gmail.com>
2021-12-27 22:58:11 +0100

    switch to server side

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -311,7 +311,7 @@ function Onboarding({ contentCards }) {
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries. See the "Technical details" section.
-export async function getStaticProps() {
+export async function getServerSideProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
   const res = await fetch('https://trello.com/b/aOOx3O4Q.json')
