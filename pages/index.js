0187b8d62cc4c81030e17ec4ed8c8190acb3cb0c
Fabien Soudiere <fsoudiere@users.noreply.github.com>
2022-01-01 17:24:46 -0600

    Missed quote

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ Home.getInitialProps = async ({ req, res }) => {
 
 if (res) {
   if (Object.keys(data).length === 0 && data.constructor === Object) {
-      res.writeHead(301, { Location: "/)
+      res.writeHead(301, { Location: "/" )
       res.end()
     }
   }
