56b1bd99617b315e9170574fb507db8c27f3c44d
Fabien Soudiere <fabiensoudiere@gmail.com>
2022-01-11 21:07:42 +0100

    noise bg

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,12 @@ export default function Layout({ children }) {
         
         <Navbar />
         <main>{children}</main>
+        <section>
+          <div className="isolate">
+            <div className="noise"></div>
+            <div className="overlay"></div>
+          </div>
+        </section>
 
       </>
     )
