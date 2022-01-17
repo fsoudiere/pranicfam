0b18e3b0201f04e82e1bc843574f69516d509e08
Fabien Soudiere <fsoudiere@users.noreply.github.com>
2022-01-17 08:51:40 -0600

    Fix text color

--- components/textColor.js
+++ components/textColor.js
@@ -3,6 +3,7 @@ import { useSpring, animated } from 'react-spring'
 export default function TextColor({className, text} ) {
   const props = useSpring({
     loop: true,
+    to: { color: 'blue',}
     from: { color: 'yellow',},
     delay: 3000,
     config: { mass: 2, tension: 2000, friction: 200 },
