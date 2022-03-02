9ab461448c079dd2cfec9293dc9cdd50189e2dd2
Fabien Soudiere <fabiensoudiere@gmail.com>
2022-03-02 19:20:52 +0100

    fixes map

--- pages/map.js
+++ pages/map.js
@@ -4,48 +4,62 @@ import styles from '../styles/Page.module.scss'
 import { Typography, Button, Grid, Stack } from '@mui/material';
 import React, { useEffect, useRef, ReactElement } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Loader from '../components/loader';
 
+const render = (status) => {
+  switch (status) {
+    case Status.LOADING:
+      return <Loader />;
+    case Status.FAILURE:
+      return <p>failed</p>;
+    case Status.SUCCESS:
+      return <MyMapComponent/>;
+  }
+};
 
-function MyMapComponent({
-  center,
-  zoom,
-}) {
+function MyMapComponent() {
+
+  const center = { lat: -34.397, lng: 150.644 };
+  const zoom = 4;
   const ref = useRef();
   
   useEffect(() => {
-    new window.google.maps.Map(ref.current, {
+    
+    const map = new window.google.maps.Map(ref.current, {
       center,
       zoom,
     });
-
+    new google.maps.Marker({
+      position: { lat: -30.397, lng: 120.644 },
+      map,
+      title: "Hello World!",
+    });
+    new google.maps.Marker({
+      position: { lat: -34.397, lng: 150.644 },
+      map,
+      title: "Hello World!",
+    });
   });
 
-  return <div ref={ref} id="map" />;
+  return <div style={{ height: '100vh', width: '100%' }} ref={ref} id="map" />;
 }
 
 function Map() {
   const apiKeyMap = process.env.NEXT_PUBLIC_GMAP;
-
-
-  const center = { lat: 102.5528, lng: 23.6345 };
-  const zoom = 4;
 
   return (
     <Layout>
     <div className={styles.container}>
     <Head>
         <title>Pranic Family - Members Map</title>
-      </Head>
+    </Head>
 
       <main className={styles.main}>
         <Typography variant='h1'>Map</Typography>
         <div className={styles.dash}></div>
-        <p className={styles.description}>
-        Let us meet</p>
-        <Wrapper apiKey={apiKeyMap}>
-          <MyMapComponent center={center} zoom={zoom}>
-          </MyMapComponent>
-        </Wrapper>
+        <p>Let us meet</p>
+
+        <Wrapper apiKey={apiKeyMap} render={render} />
 
         </main>
     </div>
